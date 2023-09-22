8f57ca35396817d4fc855c98878c92deea551f13
bzglve <viktory683@gmail.com>
2023-09-22 10:27:45 +0600

    [0.1.0] added filter and show/hide hidden

--- ui_form.py
+++ ui_form.py
@@ -17,13 +17,28 @@ class Ui_Widget(object):
     def setupUi(self, Widget):
         if not Widget.objectName():
             Widget.setObjectName(u"Widget")
-        Widget.resize(888, 600)
+        Widget.resize(800, 600)
         self.gridLayout = QGridLayout(Widget)
         self.gridLayout.setObjectName(u"gridLayout")
         self.treeView = QTreeView(Widget)
         self.treeView.setObjectName(u"treeView")
 
-        self.gridLayout.addWidget(self.treeView, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.treeView, 1, 0, 1, 1)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.lineEdit = QLineEdit(Widget)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.horizontalLayout.addWidget(self.lineEdit)
+
+        self.checkBox = QCheckBox(Widget)
+        self.checkBox.setObjectName(u"checkBox")
+
+        self.horizontalLayout.addWidget(self.checkBox)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
 
 
         self.retranslateUi(Widget)
@@ -32,6 +47,8 @@ class Ui_Widget(object):
     # setupUi
 
     def retranslateUi(self, Widget):
-        Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
+        Widget.setWindowTitle(QCoreApplication.translate("Widget", u"\u0424\u0430\u0439\u043b\u044b", None))
+        self.lineEdit.setPlaceholderText(QCoreApplication.translate("Widget", u"\u0424\u0438\u043b\u044c\u0442\u0440", None))
+        self.checkBox.setText(QCoreApplication.translate("Widget", u"\u041f\u043e\u043a\u0430\u0437\u044b\u0432\u0430\u0442\u044c \u0441\u043a\u0440\u044b\u0442\u044b\u0435", None))
     # retranslateUi
 
--- widget.py
+++ widget.py
@@ -1,27 +1,73 @@
-# This Python file uses the following encoding: utf-8
 import sys
+import pathlib
 
 from PySide2.QtWidgets import QApplication, QWidget, QFileSystemModel
-from PySide2.QtCore import QDir
+from PySide2.QtCore import QDir, QSortFilterProxyModel, QRegExp, Qt
 
 # pyside6-uic form.ui -o ui_form.py, or
 # pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_Widget
 
+class FilterProxyModel(QSortFilterProxyModel):
+    def lessThan(self, left_index, right_index):
+        left_data = left_index.data(Qt.DisplayRole)
+        right_data = right_index.data(Qt.DisplayRole)
+
+        left_is_dir = self.sourceModel().isDir(left_index)
+        right_is_dir = self.sourceModel().isDir(right_index)
+
+        if left_is_dir and not right_is_dir:
+            return True
+        elif not left_is_dir and right_is_dir:
+            return False
+        return left_data < right_data
+
+
 class Widget(QWidget):
     def __init__(self, parent=None):
-        print(type(parent))
         super().__init__(parent)
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
 
+        self.__filter_states = {
+            "default": QDir.NoDotAndDotDot | QDir.AllEntries,
+            "hidden": QDir.NoDotAndDotDot | QDir.AllEntries | QDir.Hidden
+        }
+
         dirModel = QFileSystemModel(self)
 
-        dirModel.setFilter(QDir.NoDotAndDotDot | QDir.AllDirs)
+        dirModel.setFilter(self.__filter_states["default"])
 
         dirModel.setRootPath("/")
+        self.dirModel = dirModel
 
-        self.ui.treeView.setModel(dirModel)
+        self.proxy_model = FilterProxyModel(self)
+        self.proxy_model.setSourceModel(self.dirModel)
+        self.proxy_model.setRecursiveFilteringEnabled(True)  # Enable recursive filtering
+        self.ui.treeView.setModel(self.proxy_model)
+        self.ui.treeView.setRootIndex(self.proxy_model.mapFromSource(dirModel.index(str(pathlib.Path.home()))))
+        self.ui.treeView.setSortingEnabled(True)
+
+        self.ui.checkBox.stateChanged.connect(self.toggle_hidden)
+
+        self.ui.lineEdit.textChanged.connect(self.filter_tree)
+
+    def toggle_hidden(self, state):
+        self.dirModel.setFilter(self.__filter_states[{0: "default", 2: "hidden"}[state]])
+
+    def filter_tree(self, text):
+        self.proxy_model.setFilterRegExp(QRegExp(text, Qt.CaseInsensitive))
+
+        source_index = self.proxy_model.mapToSource(self.ui.treeView.rootIndex())
+        index_item = self.dirModel.index(source_index.row(), 0, source_index.parent())
+        file_path = self.dirModel.filePath(index_item)
+        
+        # TODO костыль! - убрать
+        self.__new_root = file_path
+        if not self.__new_root:
+            self.ui.treeView.setRootIndex(self.proxy_model.mapFromSource(self.dirModel.index(self.__current_root)))
+        else:
+            self.__current_root = self.__new_root
 
 
 if __name__ == "__main__":
